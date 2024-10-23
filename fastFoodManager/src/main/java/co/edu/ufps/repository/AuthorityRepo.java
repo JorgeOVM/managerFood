06c1eb3ac3aed529fceda245b64045bd67295754
JorgeOVM <xxverasapexx@gmail.com>
2024-10-23 13:57:07 -0500

    ahora si

--- fastFoodManager/src/main/java/co/edu/ufps/repository/AuthorityRepo.java
+++
@@ -1,9 +0,0 @@
-package co.edu.ufps.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import co.edu.ufps.model.Authority;
-
-public interface AuthorityRepo extends CrudRepository<Authority, Long>{
-
-}
--- fastFoodManager/src/main/java/co/edu/ufps/repository/ProductoRepo.java
+++
@@ -1,9 +0,0 @@
-package co.edu.ufps.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import co.edu.ufps.model.Producto;
-
-public interface ProductoRepo extends CrudRepository<Producto, Integer>{
-
-}
